301f7fd23157b44778f68ced231e5606c3401398
AzureAD\MartinAmm <ma@adenin.com>
2024-11-30 09:47:28 -0500

    include thinking comment always -- not sure if that's a good idea, but valuable for debugging

--- src/artifacts/apiSample.jsx
+++ src/artifacts/apiSample.jsx
@@ -1,27 +1,39 @@
-/* <prompt>
+/* <react-artifact>
 
-show data from https://jsonplaceholder.typicode.com/users
+show data from https://jsonplaceholder.typicode.com/users in table
 
-</prompt> */
+</react-artifact> */
 
 import React from 'react';
-import ApiAccess from '@/components/apiAccess';
-
-  const UserData = () => (
+  import ApiAccess from '@/components/apiAccess';
+  
+  const UserDataTable = () => (
     <ApiAccess url="https://jsonplaceholder.typicode.com/users">
       {(data) => (
-        <div className="p-4">
-          <h1 className="text-xl font-bold mb-4">User List</h1>
-          <ul className="list-disc pl-5">
-            {data.map(user => (
-              <li key={user.id} className="mb-2">
-                {user.name} - {user.email}
-              </li>
-            ))}
-          </ul>
+        <div className="overflow-x-auto">
+          <table className="min-w-full border-collapse border border-gray-300">
+            <thead>
+              <tr className="bg-gray-200">
+                <th className="border border-gray-300 p-2">ID</th>
+                <th className="border border-gray-300 p-2">Name</th>
+                <th className="border border-gray-300 p-2">Email</th>
+                <th className="border border-gray-300 p-2">Phone</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map(user => (
+                <tr key={user.id}>
+                  <td className="border border-gray-300 p-2">{user.id}</td>
+                  <td className="border border-gray-300 p-2">{user.name}</td>
+                  <td className="border border-gray-300 p-2">{user.email}</td>
+                  <td className="border border-gray-300 p-2">{user.phone}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       )}
     </ApiAccess>
   );
-
-  export default UserData;
+  
+  export default UserDataTable;
